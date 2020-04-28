197e319fad51ca52a4e5cbe41a2c016b8ba42cb8
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 15:56:02 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,5 +2,32 @@ import pytest
 import src.exercise
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    input_values = ["names.txt","Antti","names.txt","ada","nonsense.txt","ada",""]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert output == ["Name of the file:","Search for:","Not found.",\
+                      "Name of the file:","Search for:","Found!",\
+                      "Name of the file:","Search for:","Reading the file nonsense.txt failed."]
